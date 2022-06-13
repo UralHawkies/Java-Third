7d4f9c57d6cd5c8d85f4c681ca4aa30ae4d36463
Evgeniy Panagushin <hawkies@yandex.ru>
2022-06-13 20:58:14 +0500

    Second variant

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,19 @@ public class Main {
         base_balance = 100;
 
         int added_amount; //Внесенная сумма на счет
-        added_amount = 110;
+        added_amount = 5000;
 
         int bonus; //Бонус при внесении на счет больше 1000 руб.
         bonus = added_amount / 100;
 
+        if (added_amount < 1000) {
+            System.out.println(base_balance + added_amount + " Баланс ");
+        }
+
         if (added_amount > 1000) {
-            System.out.println(base_balance + added_amount + bonus);
-        } else {
-            System.out.println(base_balance + added_amount);
+            System.out.println(base_balance + added_amount + " Баланс ");
+            System.out.println(bonus + " Бонусы ");
+            System.out.println(base_balance + added_amount + bonus + " Итого ");
         }
 
     }
