5f9c1baf2f304ae107160909499c235ee1449714
UralHawkies <105659020+UralHawkies@users.noreply.github.com>
2022-06-15 12:11:57 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int base_balance; //Первоначальный баланс счета
-        base_balance = 100;
+        int baseBalance; //Первоначальный баланс счета
+        baseBalance = 100;
 
-        int added_amount; //Внесенная сумма на счет
-        added_amount = 5000;
+        int addedAmount; //Внесенная сумма на счет
+        addedAmount = 5000;
 
         int bonus; //Бонус при внесении на счет больше 1000 руб.
-        bonus = added_amount / 100;
+        bonus = addedAmount / 100;
 
-        if (added_amount < 1000) {
+        if (addedAmount < 1000) {
             System.out.println(base_balance + added_amount + " Баланс ");
         }
 
-        if (added_amount > 1000) {
-            System.out.println(base_balance + added_amount + " Баланс ");
+        if (addedAmount > 1000) {
+            System.out.println(baseBalance + addedAmount + " Баланс ");
             System.out.println(bonus + " Бонусы ");
-            System.out.println(base_balance + added_amount + bonus + " Итого ");
+            System.out.println(baseBalance + addedAmount + bonus + " Итого ");
         }
 
     }
